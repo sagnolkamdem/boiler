0a29fab885a49c8e3833e0ce854bc4fef41154f4
sagnolKamdem <sagnolkamdem721@gmail.com>
2022-09-04 08:53:38 +0100

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,16 @@ import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ppid } from 'process';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   app.setGlobalPrefix('api');
-
+  
+  app.enableCors();
+  
   // Set configService
   const configService = app.get(ConfigService);
   const port = configService.get<string>('APP_PORT');
