373326c15d3cd3ca77d244856bb91d18825412c3
Rayan TSOLEFACK <rayanfalcao8@gmail.com>
2023-02-17 10:11:16 +0100

    minor indents

--- src/feature/score/get-all-score/get-all-score.service.ts
+++ src/feature/score/get-all-score/get-all-score.service.ts
@@ -9,124 +9,115 @@ import { GetAllScoreOutput } from './data/get-all-score.output';
 
 @Injectable()
 export class GetAllScoreService {
+  constructor(
+    @InjectRepository(User) private readonly userRepository: Repository<User>,
+    @InjectRepository(Score)
+    private readonly scoreRepository: Repository<Score>,
+  ) {}
 
-    constructor(
-        @InjectRepository(User) private readonly userRepository: Repository<User>,
-        @InjectRepository(Score) private readonly scoreRepository: Repository<Score>,
-    ) { }
-
-    async find(userId: string, startDate: Date, endDate: Date): Promise<GetAllScoreOutput> {
-        
-        try {
-
-            if (startDate && !userId) {
-
-                if (!endDate) {
-                    return {
-                        message: 'Please select an end date',
-                        statusCode: 404,
-                        data: null
-                    }
-                }
-                    
-                const user = await this.userRepository.find({
-                    where: {
-                        // Check if the current user is an administrator
-                        // role: Role.ADMIN,
-                        scores: {
-                            createdAtOfServer: Between(startDate, endDate),
-                        }
-                    },
-                    relations: {
-                        scores: true,
-                        // proofsCreatedBy: true,
-                    }
-                })
-    
-                return {
-                    message: "Data successfully retrieved",
-                    statusCode: 200,
-                    data: user,
-                };
-
-            }
-
-
-            if (userId) {
-
-
-                if (startDate) {
-
-                    if (!endDate) {
-                        return {
-                            message: 'Please select an end date',
-                            statusCode: 404,
-                            data: null
-                        }
-                    }
-                    
-                    
-                    const user = await this.userRepository.find({
-                        where: {
-                            id: userId,
-                            scores: {
-                                createdAtOfServer: Between(startDate, endDate),
-                            }
-                        },
-                        relations: {
-                            scores: true,
-                            // proofsCreatedBy: true,
-                        }
-                    })
-        
-                    return {
-                        message: "Data successfully retrieved",
-                        statusCode: 200,
-                        data: user,
-                    };
-
-                }
-
-
-                const user = await this.userRepository.find({
-                    where: {
-                        id: userId,
-                    },
-                    relations: {
-                        scores: true,
-                        // proofsCreatedBy: true,
-                    }
-                })
-    
-                return {
-                    message: "Data successfully retrieved",
-                    statusCode: 200,
-                    data: user,
-                };
-            }
-
-
-            const user = await this.userRepository.find({
-                relations: {
-                    scores: true,
-                    // proofsCreatedBy: true,
-                }
-            })
-
-            return {
-                message: "Data successfully retrieved",
-                statusCode: 200,
-                data: user,
-            };
-
-        } catch (error) {
-            console.log(error);
-            
-            return {
-                message: "An error occurred",
-                statusCode: 500,
-                data: null,
-            }
+  async find(
+    userId: string,
+    startDate: Date,
+    endDate: Date,
+  ): Promise<GetAllScoreOutput> {
+    try {
+      if (startDate && !userId) {
+        if (!endDate) {
+          return {
+            message: 'Please select an end date',
+            statusCode: 404,
+            data: null,
+          };
         }
+
+        const user = await this.userRepository.find({
+          where: {
+            // Check if the current user is an administrator
+            // role: Role.ADMIN,
+            scores: {
+              createdAtOfServer: Between(startDate, endDate),
+            },
+          },
+          relations: {
+            scores: true,
+            // proofsCreatedBy: true,
+          },
+        });
+
+        return {
+          message: 'Data successfully retrieved',
+          statusCode: 200,
+          data: user,
+        };
+      }
+
+      if (userId) {
+        if (startDate) {
+          if (!endDate) {
+            return {
+              message: 'Please select an end date',
+              statusCode: 404,
+              data: null,
+            };
+          }
+
+          const user = await this.userRepository.find({
+            where: {
+              id: userId,
+              scores: {
+                createdAtOfServer: Between(startDate, endDate),
+              },
+            },
+            relations: {
+              scores: true,
+              // proofsCreatedBy: true,
+            },
+          });
+
+          return {
+            message: 'Data successfully retrieved',
+            statusCode: 200,
+            data: user,
+          };
+        }
+
+        const user = await this.userRepository.find({
+          where: {
+            id: userId,
+          },
+          relations: {
+            scores: true,
+            // proofsCreatedBy: true,
+          },
+        });
+
+        return {
+          message: 'Data successfully retrieved',
+          statusCode: 200,
+          data: user,
+        };
+      }
+
+      const user = await this.userRepository.find({
+        relations: {
+          scores: true,
+          // proofsCreatedBy: true,
+        },
+      });
+
+      return {
+        message: 'Data successfully retrieved',
+        statusCode: 200,
+        data: user,
+      };
+    } catch (error) {
+      console.log(error);
+
+      return {
+        message: 'An error occurred',
+        statusCode: 500,
+        data: null,
+      };
     }
+  }
 }
--- src/feature/user/login-user/data/login-user.input.ts
+++ src/feature/user/login-user/data/login-user.input.ts
@@ -1,9 +1,8 @@
-import { ApiProperty } from "@nestjs/swagger";
-import { IsNotEmpty } from "class-validator";
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty } from 'class-validator';
 
 export class LoginUserInput {
-
-    @ApiProperty()
-    @IsNotEmpty()
-    token: string;
+  @ApiProperty()
+  @IsNotEmpty()
+  token: string;
 }
